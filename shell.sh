05f5146fa1c7b8e3278d8fc8e571f22d835a9838
phamthainb <phamthai4643@gmail.com>
2021-05-29 18:33:20 +0700

    Welcome 1 times
    Signed-off-by: phamthainb <phamthai4643@gmail.com>

--- shell.sh
+++ shell.sh
@@ -2,7 +2,7 @@ for i in {1..50}
 do
    echo "Welcome $i times" > $i.txt
    git add . && git commit -s -m "Welcome $i times" && git push
-   rf -rf *.txt
+   rm -rf *.txt
    echo "Welcome $i times done.................................................................."
    sleep 1s
 done
